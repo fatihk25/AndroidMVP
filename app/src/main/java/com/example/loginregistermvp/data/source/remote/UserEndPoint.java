3077974258de5f7750aea5d8abbc67e9d917ba1a
fatihk25 <fatihkhoiri25@gmail.com>
2021-10-25 18:35:32 +0700

    First Commit

---
+++ app/src/main/java/com/example/loginregistermvp/data/source/remote/UserEndPoint.java
@@ -0,0 +1,33 @@
+package com.example.loginregistermvp.data.source.remote;
+
+import com.example.loginregistermvp.data.pojo.User;
+import com.irfankhoirul.mvp_core.data.DataResult;
+
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface UserEndPoint {
+    String REGISTER_ENDPOINT = "register";
+    String LOGIN_ENDPOINT = "login";
+    String LOGOUT_ENDPOINT = "logout";
+
+    @FormUrlEncoded
+    @POST(REGISTER_ENDPOINT)
+    Call<DataResult> register(@FieldMap(encoded = true) Map<String, String> param);
+
+    @FormUrlEncoded
+    @POST(LOGIN_ENDPOINT)
+    Call<DataResult<User>> login (@FieldMap(encoded = true) Map<String, String> param);
+//    @FormUrlEncoded
+//    @POST(LOGIN_ENDPOINT)
+//    Call<DataResult<User>> login (@Field(encoded = true, value = "email") String email, @Field("password") String password);
+
+    @FormUrlEncoded
+    @POST(LOGOUT_ENDPOINT)
+    Call<DataResult> logout(@FieldMap Map<String, String> param);
+}
