26f9b7fc3055b61c15495e9b3ad58fdd910a4565
fatihk25 <fatihkhoiri25@gmail.com>
2021-10-25 22:53:31 +0700

    Change layout

--- app/src/main/java/com/example/loginregistermvp/modul/login/LoginFragment.java
+++ app/src/main/java/com/example/loginregistermvp/modul/login/LoginFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
